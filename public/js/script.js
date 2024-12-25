6342cf82b44eb4187892a32ee52275b8476790af
Krish Ahlawat <krishahlawat2520@gmail.com>
2024-12-25 14:38:47 +0530

    fixed filter options and taxed prices

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,27 @@
     );
   });
 })();
+
+let taxSwitch = document.getElementById("flexSwitchCheckDefault");
+taxSwitch.addEventListener("click", function () {
+  let taxInfo = document.querySelectorAll(".tax-info");
+  let taxedPrice = document.querySelectorAll(".price");
+  for (let i = 0; i < taxedPrice.length; i++) {
+    let priceText = taxedPrice[i].getAttribute("data-original-price");
+    if (!priceText) {
+      priceText = taxedPrice[i].innerText.replace(/[^\d]/g, "");
+      taxedPrice[i].setAttribute("data-original-price", priceText);
+    }
+    let price = parseFloat(priceText);
+    if (!isNaN(price)) {
+      let taxedAmount = Math.round(price * 1.18).toLocaleString("en-IN");
+      if (taxSwitch.checked) {
+        taxedPrice[i].innerText = `₹ ${taxedAmount} / night with taxes`;
+      } else {
+        taxedPrice[i].innerText = `₹ ${price.toLocaleString(
+          "en-IN"
+        )} / night without taxes`;
+      }
+    }
+  }
+});
